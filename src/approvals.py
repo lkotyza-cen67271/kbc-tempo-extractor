a6c09ed68f28b1ef309f04aa3c65172a30f963d5
Lukas Kotyza <lkotyza@csas.cz>
2024-11-21 12:48:17 +0100

    chore: python lint

--- src/approvals.py
+++ src/approvals.py
@@ -98,7 +98,8 @@ def run(since: datetime) -> tuple[list[dict], list[dict]]:
         while period_start_date < datetime.now():
             period = tempo.team_timesheet_approvals(team['id'], str(period_start_date.date()))
             if period is None:
-                logging.error(f"Period is None - skipping team (team: {team['id']} - {team['name']}; period_start: {str(period_start_date.date())})")
+                logging.error("Period is None - skipping team")
+                logging.error(f"(team: {team['id']} - {team['name']}; period_start: {str(period_start_date.date())})")
                 break
             raw_out.extend(period)
             next_period_start_date = _next_period_start_from_current(period)
